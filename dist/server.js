bc0eb5d8446fcbf2d77b7486eba8aec68d992cff
Lucas Vida <lucasvida@gmail.com>
2025-12-03 20:00:06 -0300

    Ajuste

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import env from "dotenv";
-env.config();
-const app = express();
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
